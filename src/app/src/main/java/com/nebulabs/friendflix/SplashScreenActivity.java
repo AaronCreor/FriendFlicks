3c2faa5e3a7fd7b63263d760c630e5c028489a99
Chinmay.Sharma <chinmay.sharma@student.ufv.ca>
2020-12-02 10:43:47 +0530

    add splash screen

---
+++ src/app/src/main/java/com/nebulabs/friendflix/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.nebulabs.friendflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }, 2000);
+    }
+}
