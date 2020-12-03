c5012b6c16c71e968dd5f11264a3ac330a0e70fa
Karan Bhullar <lumbywho@outlook.com>
2020-12-04 02:04:18 +0530

    Update Profile Screen (change logout button style + remove redundant settings button)

--- src/app/src/main/java/com/nebulabs/friendflix/MyListFragment.java
+++ src/app/src/main/java/com/nebulabs/friendflix/MyListFragment.java
@@ -26,7 +26,7 @@ public class MyListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recycler_mylist,container,false);
+        return inflater.inflate(R.layout.fragment_mylist,container,false);
     }
 
     @Override
