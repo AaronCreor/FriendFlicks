4eb60c22cc128a077b42eedd22b12296bcda5eb7
Chinmay.Sharma <chinmay.sharma@student.ufv.ca>
2020-12-04 13:06:59 +0530

    fix: back press button and tab icon change

--- src/app/src/main/java/com/nebulabs/friendflix/MainActivity.java
+++ src/app/src/main/java/com/nebulabs/friendflix/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nebulabs.friendflix;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.KeyEvent;
 import androidx.annotation.NonNull;
@@ -11,27 +12,11 @@ import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import es.dmoral.toasty.Toasty;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public void replaceFragments(Class fragmentClass) {
-        Fragment fragment = null;
-        try {
-            fragment = (Fragment) fragmentClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragment_container_view, fragment)
-                .commit();
-    }
-    @Override
-    public void onBackPressed() {
-        Fragment selectedFrag = null;
-        selectedFrag = new MyListFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view,selectedFrag).commit();
-    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomTabView = findViewById(R.id.bottom_navigation);
         bottomTabView.setOnNavigationItemSelectedListener(navListener);
+
+        bottomTabView.getMenu().getItem(0).setChecked(true);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view,new MyListFragment()).commit();
 
@@ -67,5 +54,47 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+/*
+    public void replaceFragments(Class fragmentClass) {
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container_view, fragment)
+                .commit();
+    }
+*/
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toasty.info(this, "Please click BACK again to exit", Toasty.LENGTH_SHORT).show();
+
+
+        BottomNavigationView bottomTabView = findViewById(R.id.bottom_navigation);
+        bottomTabView.getMenu().getItem(0).setChecked(true);
+
+        Fragment selectedFrag = null;
+        selectedFrag = new MyListFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view,selectedFrag).commit();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
 
 }
