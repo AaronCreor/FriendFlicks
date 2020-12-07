258550b7464e1c05b1b083b5a7c4219610b2984a
Chinmay.Sharma <chinmay.sharma@student.ufv.ca>
2020-12-07 20:38:07 +0530

    fixed merge bugs

--- src/app/src/main/java/com/nebulabs/friendflix/User.java
+++ src/app/src/main/java/com/nebulabs/friendflix/User.java
@@ -5,47 +5,47 @@ import com.google.gson.annotations.SerializedName;
 /**
  * User class
  */
-public class User {
-
-    @SerializedName("uid")
-    String uid;
-
-    @SerializedName("userEmail")
-    String userEmail;
-
-
-    /**
-     * Create a new user
-     * @param mUid user id
-     * @param mUserEmail user email
-     */
-    public User(String mUid, String mUserEmail) {
-        this.userEmail = mUserEmail;
-        this.uid = mUid;
-    }
-
-    /**
-     * Create a new user
-     * @param mUid user id
-     */
-    public User(String mUid){
-        this.uid = mUid;
-    }
-
-    /**
-     * Add movie to user database
-     * @param mx movie object
-     */
-    public void addMovie(Movie mx) {
-        Movie input = mx;
-        // TODO: ADD MOVIE TO DB
-    }
-
-    /**
-     * Retrieve all movies from database
-     */
-    public void getMovies(){
-        // TODO: GET USER MOVIES
-    }
-
-}
+//public class User {
+//
+//    @SerializedName("uid")
+//    String uid;
+//
+//    @SerializedName("userEmail")
+//    String userEmail;
+//
+//
+//    /**
+//     * Create a new user
+//     * @param mUid user id
+//     * @param mUserEmail user email
+//     */
+//    public User(String mUid, String mUserEmail) {
+//        this.userEmail = mUserEmail;
+//        this.uid = mUid;
+//    }
+//
+//    /**
+//     * Create a new user
+//     * @param mUid user id
+//     */
+//    public User(String mUid){
+//        this.uid = mUid;
+//    }
+//
+//    /**
+//     * Add movie to user database
+//     * @param mx movie object
+//     */
+//    public void addMovie(Movie mx) {
+//        Movie input = mx;
+//        // TODO: ADD MOVIE TO DB
+//    }
+//
+//    /**
+//     * Retrieve all movies from database
+//     */
+//    public void getMovies(){
+//        // TODO: GET USER MOVIES
+//    }
+//
+//}
